3d6f6c3fb5edbac440f5fc59a4c311db34c89a5c
Kapakahi <rubbishman@gmail.com>
2020-04-16 13:11:00 +0900

    Finished a bit more, not done yet though

--- scripts/selecting/allCheckinInOn.sql
+++ scripts/selecting/allCheckinInOn.sql
@@ -1,1 +1,5 @@
---  your SQL here
+SELECT first_name, last_name, date_of_birth, gender, town_of_origin, checked_in_at
+FROM students INNER JOIN checkins
+ON students.id = checkins.student_id;
+WHERE checkins.checked_in_at >= '2016-06-01'
+AND checkins.checked_in_at < '2016-07-01';              
--- scripts/selecting/allStudentAndCheckin.sql
+++ scripts/selecting/allStudentAndCheckin.sql
@@ -1,1 +1,3 @@
--- Your SQL here
+SELECT first_name, last_name, date_of_birth, gender, town_of_origin, student_id, checked_in_at
+FROM students INNER JOIN checkins
+ON students.id = checkins.student_id;
