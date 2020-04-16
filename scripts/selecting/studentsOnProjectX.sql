2e67b9ea8d4fbcd75332c269814cd00a5e49102e
Kapakahi <rubbishman@gmail.com>
2020-04-17 00:17:55 +0900

    Almost finished w/ basics

--- scripts/selecting/studentsOnProjectX.sql
+++ scripts/selecting/studentsOnProjectX.sql
@@ -1,1 +1,7 @@
--- Your SQL here
+SELECT students.first_name, students.last_name 
+FROM students
+  INNER JOIN students_to_project as sp 
+  ON students.id = sp.student_id
+  INNER JOIN projects as p
+  ON p.id = sp.project_id
+WHERE p.id = '5';
